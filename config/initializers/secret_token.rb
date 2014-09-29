325ce57f8fad7ba2653ab436a7b39c0ecf40ab1a
Liu Xuefeng <liuxf@jaist.ac.jp>
2014-09-29 17:00:16 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KenshohinBlog::Application.config.secret_key_base = '4dbb96c0c30e0de5d4c36841e4313f70da7e9cbf16242cdcbdce68dc37c41e381d610a9221eae17104d6caa78283b46fe4678b2fd51f721bd60e63538a61467f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KenshohinBlog::Application.config.session_store :cookie_store, key: '_kenshohin_blog_session'
